2eea1b1a7fb500868c258dab33069ca885a42574
muhammadazra4503 <azirlol023@gmail.com>
2021-09-27 15:41:53 +0700

    HelloWorld.java

---
+++ Praktikum/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) { 
+        System.out.println("Nama:Muhammad Azra");
+		System.out.println("NIM:6706213046");
+		System.out.println("Kelas:D3IF-45-03");
+    } 
+} 	
